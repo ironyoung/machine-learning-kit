5ce068cf9802136f6f9da36dec917005ca54f230
unknown <>
2016-06-06 19:25:16 +0800

    logistic regression

---
+++ LogisticRegression/horseColic_logReg.py
@@ -0,0 +1,58 @@
+from logReg import *
+
+def classifyVector(inX, weights):
+	prob = sigmoid(sum(inX * weights))
+	if prob > 0.5:
+		return 1.0
+	else:
+		return 0.0
+
+
+def colicTest():
+	frTrain = open('horseColicTraining.txt')
+	frTest	= open('horseColicTest.txt')
+	trainingSet		= []
+	trainingLabel	= []
+
+	for line in frTrain.readlines():
+		currLine= line.strip().split('\t')
+		lineArr	= []
+
+		for i in range(21):
+			lineArr.append(float(currLine[i]))
+
+		trainingSet.append(lineArr)
+		trainingLabel.append(float(currLine[21]))
+
+	trainWeights= stocGradAscent1(array(trainingSet), trainingLabel, 500)
+	errorCount 	= 0
+	numTestVec	= 0.0
+
+	for line in frTest.readlines():
+		numTestVec	+= 1.0
+		currLine	= line.strip().split('\t')
+		lineArr		= []
+		for i in range(21):
+			lineArr.append(float(currLine[i]))
+		if int(classifyVector(array(lineArr), trainWeights)) != int(currLine[21]):
+			errorCount	+= 1
+
+	errorRate	= float(errorCount)/numTestVec
+	print "the error rate of this test is: %f" % errorRate
+	return errorRate
+
+
+def multiTest():
+	numTests	= 10
+	errorRateSum= 0.0
+	for k in range(numTests):
+		errorRateSum	+= colicTest()
+	print "after %d iterations the average error rate is: %f" %(numTests, errorRateSum/float(numTests))
+
+
+def main():
+	multiTest()
+
+
+if __name__ == '__main__':
+	main()
---
+++ LogisticRegression/logReg.py
@@ -0,0 +1,57 @@
+from numpy import *
+
+def loadDataSet():
+	dataMat  = [];
+	labelMat = [];
+	fr = open('testSet.txt')
+	for line in fr.readlines():
+		lineArr = line.strip().split()
+		dataMat.append([1.0,  float(lineArr[0]), float(lineArr[1])])
+		labelMat.append(int(lineArr[2]))
+	return dataMat, labelMat
+
+
+def sigmoid(inX):
+	return 1.0/(1.0 + exp(-inX))
+
+
+def gradAscent(dataMatIn, classLabels):
+	dataMatrix 	= mat(dataMatIn)
+	labelMat 	= mat(classLabels).transpose()
+	m, n 	= shape(dataMatrix)
+	alpha	= 0.001
+	maxCycles = 500
+	weights = ones((n, 1))
+	for k in range(maxCycles):
+		h 	= sigmoid(dataMatrix * weights)
+		error 	= (labelMat - h)
+		weights = weights + alpha * dataMatrix.transpose() * error
+	return weights
+
+
+def stocGradAscent0(dataMatrix, classLabels):
+	dataMatrix	= array(dataMatrix)
+	m, n 	= shape(dataMatrix)
+	alpha	= 0.01
+	weights = ones(n)
+	for i in range(m):
+		h = sigmoid(sum(dataMatrix[i] * weights))
+		error 	= classLabels[i] - h
+		weights	= weights + alpha * error * dataMatrix[i]
+	return weights
+
+
+def stocGradAscent1(dataMatrix, classLabels, numIter=150):
+	dataMatrix	= array(dataMatrix)
+	m, n 	= shape(dataMatrix)
+	weights = ones(n)
+	for j in range(numIter):
+		dataIndex	= range(m)
+		for i in range(m):
+			alpha = 4 / (1.0 + i + j) + 0.01	# modify learning rate "alpha" every iteration
+			randIndex = int(random.uniform(0, len(dataIndex)))
+			h 	= sigmoid(sum(dataMatrix[randIndex] * weights))
+			error = classLabels[randIndex] - h
+			weights	= weights + alpha * error * dataMatrix[randIndex]
+			del(dataIndex[randIndex])
+	return weights
